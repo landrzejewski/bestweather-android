c255781cc54c22173dea979137909cfb4b9126c5
landrzejewski <landrzejewski.poczta@gmail.com>
2023-12-13 16:18:23 +0100

    Theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.compose.material3:material3")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/pl/training/bestweather/MainActivity.kt
+++ app/src/main/java/pl/training/bestweather/MainActivity.kt
@@ -1,35 +1,27 @@
 package pl.training.bestweather
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.compose.runtime.Composable
 import dagger.hilt.android.AndroidEntryPoint
-import pl.training.bestweather.databinding.ActivityMainBinding
+import androidx.compose.material3.Text
+import androidx.lifecycle.viewmodel.compose.viewModel
+import pl.training.bestweather.forecast.adapters.view.ForecastViewModel
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        setContent {
+            Forecast()
+        }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    @Composable
+    fun Forecast(viewModel: ForecastViewModel = viewModel()) {
+        Text(text = "Forecast")
     }
 
 }
