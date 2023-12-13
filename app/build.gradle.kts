dc39fee0bfb68db3688812e71d5d89287a070b39
landrzejewski <landrzejewski.poczta@gmail.com>
2023-12-13 15:53:02 +0100

    Theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,10 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     hilt {
         enableAggregatingTask = true
@@ -46,6 +50,13 @@ android {
 dependencies {
     val hiltVersion = "2.49"
     val roomVersion = "2.6.1"
+
+    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
