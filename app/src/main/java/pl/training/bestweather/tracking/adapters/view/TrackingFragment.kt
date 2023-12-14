e9c08496ec457e6e535d0614fb0cde71e394d5a6
landrzejewski <landrzejewski.poczta@gmail.com>
2023-12-14 12:04:39 +0100

    x

--- app/src/main/java/pl/training/bestweather/tracking/adapters/view/TrackingFragment.kt
+++ app/src/main/java/pl/training/bestweather/tracking/adapters/view/TrackingFragment.kt
@@ -2,7 +2,9 @@ package pl.training.bestweather.tracking.adapters.view
 
 import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper.getMainLooper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -10,19 +12,28 @@ import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.fragment.app.Fragment
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices.getFusedLocationProviderClient
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.maps.CameraUpdateFactory.newLatLngZoom
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import pl.training.bestweather.R
 import pl.training.bestweather.databinding.FragmentTrackingBinding
 
-// https://android-developers.googleblog.com/2017/11/moving-past-googleapiclient_21.html
 class TrackingFragment : Fragment() {
 
     private lateinit var binding: FragmentTrackingBinding
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var map: GoogleMap
+    private val locationRequest = LocationRequest.Builder(1_000)
+        .setPriority(PRIORITY_HIGH_ACCURACY)
+        .setMinUpdateDistanceMeters(5F)
+        .build()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         super.onCreateView(inflater, container, savedInstanceState)
@@ -37,14 +48,33 @@ class TrackingFragment : Fragment() {
         mapFragment.getMapAsync(onMapReady)
     }
 
-    private val onMapReady = OnMapReadyCallback {
-        map = it
+    override fun onResume() {
+        super.onResume()
         requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
     }
 
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(onLocationUpdate)
+    }
+
+    private val onMapReady = OnMapReadyCallback {
+        map = it
+    }
+
+    private val onLocationUpdate =  object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            for (location in locationResult.locations){
+               Log.i("###", location.toString())
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
     private val requestPermissionLauncher = registerForActivityResult(RequestPermission()) { isGranted ->
         if (isGranted) {
             configureMap()
+            fusedLocationClient.requestLocationUpdates(locationRequest, onLocationUpdate, getMainLooper())
         } else {
             Log.i("###", "User denied location access")
         }
@@ -55,10 +85,19 @@ class TrackingFragment : Fragment() {
         map.isMyLocationEnabled = true
         map.uiSettings.isCompassEnabled = true
         map.uiSettings.isMyLocationButtonEnabled = true
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener {
-               Log.i("###", it.toString())
-            }
+        fusedLocationClient.lastLocation.addOnSuccessListener(::moveCamera)
+    }
+
+    private fun moveCamera(location: Location) {
+        val position = LatLng(location.latitude, location.longitude)
+        val positionUpdate = newLatLngZoom(position, CAMERA_ZOOM)
+        map.animateCamera(positionUpdate)
+    }
+
+    companion object {
+
+        const val CAMERA_ZOOM = 16F
+
     }
 
 }
