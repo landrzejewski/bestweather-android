fdacaab7d157ae6394cdd70c6ff01c44988dbc7e
landrzejewski <landrzejewski.poczta@gmail.com>
2023-12-14 11:35:13 +0100

    x

--- app/src/main/java/pl/training/bestweather/tracking/adapters/view/TrackingFragment.kt
+++ app/src/main/java/pl/training/bestweather/tracking/adapters/view/TrackingFragment.kt
@@ -7,24 +7,27 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices.getFusedLocationProviderClient
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import pl.training.bestweather.R
 import pl.training.bestweather.databinding.FragmentTrackingBinding
 
-
+// https://android-developers.googleblog.com/2017/11/moving-past-googleapiclient_21.html
 class TrackingFragment : Fragment() {
 
     private lateinit var binding: FragmentTrackingBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var map: GoogleMap
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = FragmentTrackingBinding.inflate(layoutInflater)
+        fusedLocationClient = getFusedLocationProviderClient(requireActivity())
         return binding.root
     }
 
@@ -34,13 +37,9 @@ class TrackingFragment : Fragment() {
         mapFragment.getMapAsync(onMapReady)
     }
 
-    override fun onResume() {
-        super.onResume()
-        requestPermissionLauncher.launch(ACCESS_FINE_LOCATION);
-    }
-
     private val onMapReady = OnMapReadyCallback {
         map = it
+        requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
     }
 
     private val requestPermissionLauncher = registerForActivityResult(RequestPermission()) { isGranted ->
@@ -56,6 +55,10 @@ class TrackingFragment : Fragment() {
         map.isMyLocationEnabled = true
         map.uiSettings.isCompassEnabled = true
         map.uiSettings.isMyLocationButtonEnabled = true
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener {
+               Log.i("###", it.toString())
+            }
     }
 
 }
