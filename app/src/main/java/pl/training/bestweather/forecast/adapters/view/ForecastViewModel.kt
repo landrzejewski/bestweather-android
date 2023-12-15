a14ab7ad4fd51639474dca6c4c689877edaa9969
landrzejewski <landrzejewski.poczta@gmail.com>
2023-12-15 10:49:27 +0100

    co

--- app/src/main/java/pl/training/bestweather/forecast/adapters/view/ForecastViewModel.kt
+++ app/src/main/java/pl/training/bestweather/forecast/adapters/view/ForecastViewModel.kt
@@ -1,5 +1,6 @@
 package pl.training.bestweather.forecast.adapters.view
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
